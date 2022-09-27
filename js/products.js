76eeaa45d8bf4bec258826c306fdcba405928aab
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-27 05:25:09 +0200

    Function added to get all of products

--- js/products.js
+++ js/products.js
@@ -3,12 +3,25 @@
 *                                       Fetch data from Products API
 
 ****************************************************************************************************************** */
+
 async function getNumOfTotalProducts() {
     let url = 'https://dummyjson.com/products?limit=0&select=total';
     const res = await fetch(url);
     try {
         const product = await res.json();
         return product.total
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+async function getAllProducts() {
+    const num = await getNumOfTotalProducts()
+    let url = 'https://dummyjson.com/products?limit=' + num;
+    const res = await fetch(url);
+    try {
+        const products = await res.json();
+        return products
     } catch (error) {
         console.log("error", error);
     }
