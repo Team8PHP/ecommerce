0e0157eacc8e07686be5e140958dfa2f5202116d
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-28 01:43:05 +0200

    add event to capture input of user and show result

--- js/modules/search-products.js
+++ js/modules/search-products.js
@@ -4,19 +4,39 @@
 
 ****************************************************************************************************************** */
 
-import { getAllProducts} from "./prdoucts-api.js"
+import { getAllProducts } from "./prdoucts-api.js"
 
 function filterItems(arr, query) {
     return arr.filter((element) => element.title.toLowerCase().includes(query.toLowerCase()));
 }
 
-export function searchBar (bar_id ,items_id,ref_section_id,search_buffer){
+export function searchBar(bar_id, items_id, ref_section_id, search_buffer) {
     const searchBar = document.getElementById(bar_id)
     searchBar.addEventListener("focus", async function () {
         if (Object.keys(searchProduct).length === 0) {
             search_buffer = await getAllProducts();
         } else {
             return
+        }
+    })
+    searchBar.addEventListener("keyup", async function () {
+        let query = searchBar.value;
+        let filteredProducts = filterItems(search_buffer.products, query);
+        let itemsDiv = document.getElementById(items_id)
+        itemsDiv.replaceChildren();
+        for (let i = 0; i < filteredProducts.length; i++) {
+            let newItem = document.createElement("h6");
+            let newItemLink = document.createElement("a");
+            newItemLink.innerHTML = filteredProducts[i].title;
+            newItemLink.href = ref_section_id
+            newItem.append(newItemLink)
+            itemsDiv.append(newItem)
+            newItemLink.addEventListener("click", (event) => {
+                event.stopPropagation();
+                console.log("hi");
+                window.location.href = `./test.html?product-id=${filteredProducts[i].id}&category=${filteredProducts[i].category}`;
+            })
+            if (i === 3) { break; }
         }
     })
 }
