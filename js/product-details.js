7cecfc5cb3ffcee497de190cfdfbf286d3007a92
Mnabil10 <nabilm604@gmail.com>
2022-09-27 20:56:46 +0200

    Create Function to get Product Details

--- js/product-details.js
+++ js/product-details.js
@@ -14,4 +14,18 @@ const toDoArray = JSON.parse(localStorage.getItem('to-do-list')) || [];
 ////////
 var detils = fetch(`https://dummyjson.com/products/${id}`) //Fetch API To git Product by ID 
 .then((data) =>{return data.json();})
-.then();
+.then(productDetails =>{         //Create Function to git product details
+    //console.log(productDetails.title);
+        let makeup = `<div>
+        <h2>${productDetails.title}</h2>
+        <img src="${productDetails.thumbnail}" alt="">
+        <p>${productDetails.price}$</p>
+        <p>${productDetails.description}</p>
+        <div>
+        <img src="${productDetails.images[0]}" alt="">
+        <img src="${productDetails.images[1]}" alt="">
+        <img src="${productDetails.images[2]}" alt="">
+        </div>
+        </div>`
+        document.getElementById("product").insertAdjacentHTML('afterbegin',makeup);
+});
