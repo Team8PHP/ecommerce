109678b77e16580837969503bb58167b155d9d1a
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-28 01:40:44 +0200

    add function to filter items

--- js/modules/search-products.js
+++ js/modules/search-products.js
@@ -5,3 +5,7 @@
 ****************************************************************************************************************** */
 
 import { getAllProducts} from "./prdoucts-api.js"
+
+function filterItems(arr, query) {
+    return arr.filter((element) => element.title.toLowerCase().includes(query.toLowerCase()));
+}
