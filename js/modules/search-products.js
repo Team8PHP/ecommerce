c9e23018972a4be6646091ab6db3934dea716a96
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-28 01:42:05 +0200

    add event to search bar on focus to retrive data

--- js/modules/search-products.js
+++ js/modules/search-products.js
@@ -9,3 +9,14 @@ import { getAllProducts} from "./prdoucts-api.js"
 function filterItems(arr, query) {
     return arr.filter((element) => element.title.toLowerCase().includes(query.toLowerCase()));
 }
+
+export function searchBar (bar_id ,items_id,ref_section_id,search_buffer){
+    const searchBar = document.getElementById(bar_id)
+    searchBar.addEventListener("focus", async function () {
+        if (Object.keys(searchProduct).length === 0) {
+            search_buffer = await getAllProducts();
+        } else {
+            return
+        }
+    })
+}
