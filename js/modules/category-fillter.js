cbe096f9af4fd83be348a37cdd9a4b7f34f0af45
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-28 02:33:20 +0200

    add create fillter function

--- js/modules/category-fillter.js
+++ js/modules/category-fillter.js
@@ -6,3 +6,22 @@
 
 import { getCategories , getCategoryProducts } from "./products-api.js"
 import { showProducts} from "./show-products"
+
+export async function createCategoryFilter() {
+    const categories = await getCategories();
+    const categoriesDiv = document.getElementById("categories");
+    const categoriesList = document.createElement("ul");
+
+    const allProducts = document.createElement("li");
+    allProducts.setAttribute('id', 'category-all');
+    const allProductsLink = document.createElement("a");
+    allProductsLink.innerHTML = "All Products";
+    allProductsLink.href = "#products-section"
+    allProductsLink.addEventListener("click", showALLProducts)
+    allProducts.append(allProductsLink)
+    categoriesList.append(allProducts);
+
+
+    createCategoryListItems(categories, categoriesList);
+    categoriesDiv.append(categoriesList);
+}
