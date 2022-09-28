51038320934a30d6c957d7916d24623fe8d8f975
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-28 02:30:18 +0200

    added show all products

--- js/products.js
+++ js/products.js
@@ -1,11 +1,12 @@
 import {getAllProducts,getCategoryProducts,getCategories} from "./modules/products-api.js"
 import {searchBar} from "./modules/search-products.js"
-
+import {showALLProducts} from "./modules/show-products.js"
 let searchProduct = {}
 
 
 function os (){
-    searchBar('searchBar','search-items','#products-section',searchProduct)
+    searchBar('searchBar','search-items','#products-section',searchProduct);
+    showALLProducts();  
 }
 
 os();
