40af03b3d6dd2d59580f52e956b5fd93b6b3032b
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-28 02:18:12 +0200

    added show all products and create card functions

--- js/modules/show-products.js
+++ js/modules/show-products.js
@@ -3,7 +3,9 @@
 *                                       Show Products Module
 
 ****************************************************************************************************************** */
-function showProducts(showedProducts) {
+import {getAllProducts} from './products-api.js';
+
+export function showProducts(showedProducts) {
     const allProductsDiv = document.getElementById("products-section");
     allProductsDiv.replaceChildren();
     let viewMoreButton = null;
@@ -17,4 +19,40 @@ function showProducts(showedProducts) {
         })
     }
 
+}
+
+export async function showALLProducts() {
+    const products = await getAllProducts()
+    showProducts(products);
+}
+
+function createProductCard(parentDiv, product) {
+    parentDiv.innerHTML += `
+    <div class="col-md-6 col-lg-3 my-4 mb-md-0  wow fadeInUp"> 
+        <div class="card" id='product-${product.id}'>
+            <div class="overflow-hidden">
+                <img src= ${product.thumbnail}
+                class="card-img-top" style="height:20rem" alt="Gaming Laptop" id="product-img-${product.id}"/>
+            </div>
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <p class="small"><a href="#!" class="text-muted">${product.category}</a></p>
+                </div>
+
+                <div class="d-flex justify-content-between mb-3">
+                    <h5 class="mb-0">${product.title}</h5>
+                    <h5 class="text-dark mb-0">$ ${product.price}</h5>
+                </div>
+
+                <div class="d-flex justify-content-between mb-2">
+                    <p class="text-muted mb-0">Available: <span class="fw-bold">${product.stock}</span></p>
+                </div>
+                <div class="d-flex justify-content-around p-3">
+                    <button class="btn btn-primary px-3" id="more-details-btn-${product.id}">more details</button>
+                    <button class="btn px-3" id="cart-btn-${product.id}">add to cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+   `
 }
