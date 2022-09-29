1439a19d76eb9833289e6f62bd292b9684284567
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-09-29 20:18:17 +0200

    add event listener to card button

--- js/modules/show-products.js
+++ js/modules/show-products.js
@@ -64,12 +64,14 @@ function showSelectedProducts(products, productsSectionID) {
     allProductsDiv.replaceChildren();
     let viewMoreButton = null;
     let categoryLink= null;
+    let cartButton = null;
     products.forEach((product) => {
         createProductCard(allProductsDiv, product);
     })
     for (let i = 0; i < products.length; i++) {
         viewMoreButton = document.getElementById('more-details-btn-' + products[i].id)
         categoryLink= document.getElementById('product-category-' + products[i].id)
+        cartButton = document.getElementById('cart-btn-' + products[i].id)
         viewMoreButton.addEventListener("click", () => {
             window.location.href = `./product-details.html?product-id=${products[i].id}&category=${products[i].category}`;
         })
@@ -77,6 +79,9 @@ function showSelectedProducts(products, productsSectionID) {
             let choosedProducts =  await getCategoryProducts(products[i].category)
             console.log();
             showProductsWithSlider(choosedProducts, "products-section");
+        })
+        cartButton.addEventListener("click",()=>{
+            pagesAddToCart(products[i]);
         })
 
     }
