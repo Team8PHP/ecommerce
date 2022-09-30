69f399da26a7414d06737f02a43d39b356cfb7be
Mohamed Talaat <mohamed_talaat_47@yahoo.com>
2022-09-30 02:41:19 +0200

    cart style added

--- js/cart.js
+++ js/cart.js
@@ -21,7 +21,7 @@ document.getElementsByClassName('btn-purchase')[0].addEventListener('click', pur
 
 // alert purchase complete and clear cart 
 function purchaseClicked() {
-    alert('Thank you for your purchase')
+    $("#exampleModal").modal();
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
@@ -32,46 +32,46 @@ function purchaseClicked() {
 //  remove the parent div containing product 
 function removeCartItem(event) {
     var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
+    buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
     updateCartTotal()
     decrease()
     localStorage.clear()
-//  
-//   console.log(buttonClicked.parentElement.parentElement.children[0].children[1].innerText)
-//   console.log(buttonClicked.parentElement.parentElement.children[0].children[0].src)  
-//   console.log(buttonClicked.parentElement.parentElement.children[1].innerText) 
- 
-var newproducts = [ ]
-// console.log(cart_items.children)
-let arr =cart_items.children
-for(let i=0; i< arr.length;i++){
-    // console.log(arr[i].children[0].children[1].innerText)
-    // console.log(arr[i].children[0].children[0].src)
-    // console.log(arr[i].children[1].innerText)
-    var newobj =
-        {title: arr[i].children[0].children[1].innerText,
-            price:arr[i].children[1].innerText,
-            thumbnail:arr[i].children[0].children[0].src
-            }
-            newproducts.push(newobj)
-  }
-localStorage.setItem('products',JSON.stringify(newproducts))
-// console.log(newproducts)
+    //  
+    //   console.log(buttonClicked.parentElement.parentElement.children[0].children[1].innerText)
+    //   console.log(buttonClicked.parentElement.parentElement.children[0].children[0].src)  
+    //   console.log(buttonClicked.parentElement.parentElement.children[1].innerText) 
 
- var itemget =JSON.parse(localStorage.getItem('products'))
-//  console.log(itemget)
+    var newproducts = []
+    // console.log(cart_items.children)
+    let arr = cart_items.children
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(arr[i].children[0].children[1].innerText)
+        // console.log(arr[i].children[0].children[0].src)
+        // console.log(arr[i].children[1].innerText)
+        var newobj =
+        {
+            title: arr[i].children[0].children[1].innerText,
+            price: arr[i].children[1].innerText,
+            thumbnail: arr[i].children[0].children[0].src
+        }
+        newproducts.push(newobj)
+    }
+    localStorage.setItem('products', JSON.stringify(newproducts))
+    // console.log(newproducts)
 
-    for(let i=0 ;i<itemget.length;i++)
-     {
-       var title = itemget[i].title
-       var price =  itemget[i].price 
-       var imageSrc =itemget[i].thumbnail
-       console.log(title,price,imageSrc)
-       
+    var itemget = JSON.parse(localStorage.getItem('products'))
+    //  console.log(itemget)
+
+    for (let i = 0; i < itemget.length; i++) {
+        var title = itemget[i].title
+        var price = itemget[i].price
+        var imageSrc = itemget[i].thumbnail
+        console.log(title, price, imageSrc)
+
     }
 
- }
-    
+}
+
 // number of item in cart  can be only number and  >0
 function quantityChanged(event) {
     var input = event.target
@@ -83,34 +83,25 @@ function quantityChanged(event) {
 
 // get title , price and image of product and send to 3 functions 
 function addToCartClicked(event) {
-    var item =JSON.parse(localStorage.getItem('products'))
+    var item = JSON.parse(localStorage.getItem('products'))
     console.log(item)
-    for(let i=0 ;i<item.length;i++)
-     {
-       var title = item[i].title
-       var price =  item[i].price 
-       var imageSrc =item[i].thumbnail
-    //    console.log(title,price,imageSrc)
-       addItemToCart(title, price, imageSrc)
-       updateCartTotal()
-       increase()
-      
+    for (let i = 0; i < item.length; i++) {
+        var title = item[i].title
+        var price = item[i].price
+        var imageSrc = item[i].thumbnail
+        //    console.log(title,price,imageSrc)
+        addItemToCart(title, price, imageSrc)
+        updateCartTotal()
+        increase()
+
     }
     // console.log(title,price,imageSrc)
-    
+
 }
 
 // add new div containing product details and check if item is already added alert the user
 function addItemToCart(title, price, imageSrc) {
     var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    cartRow.classList.add('p-2')
-    cartRow.classList.add('d-flex')
-    cartRow.classList.add('flex-row')
-    cartRow.classList.add('align-items-center')
-    cartRow.classList.add('justify-content-around')
-    cartRow.classList.add('col-12')
-    cartRow.classList.add('border')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
     for (var i = 0; i < cartItemNames.length; i++) {
@@ -120,19 +111,34 @@ function addItemToCart(title, price, imageSrc) {
         }
     }
     var cartRowContents = `
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="300" height="150">
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <span class="cart-sub-price cart-column"></span>
-        <input class="cart-quantity-input" type="number" value="1">
-        <div class="cart-quantity cart-column">
-            <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`
+
+    <div class="row mb-4 d-flex justify-content-between align-items-center cart-row">
+    <div class="col-md-2 col-lg-2 col-xl-2">
+        <img src="${imageSrc}"
+            class="img-fluid rounded-3" alt="${title}">
+    </div>
+    <div class="col-md-3 col-lg-3 col-xl-3">
+        <h6 class="text-black mb-0">${title}</h6>
+    </div>
+    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+
+        <input id="form1" min="0" name="quantity" value="1" type="number"
+            class="form-control form-control-sm cart-quantity-input" />
+
+    </div>
+    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+        <h6 class="mb-0 cart-price">$ ${price}</h6>
+    </div>
+    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+        <a href="#!" class="text-muted btn-remove-cart"><i class="fas fa-times"></i></a>
+    </div>
+</div>
+
+<hr class="my-4">
+        `
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('btn-remove-cart')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
@@ -151,48 +157,51 @@ function updateCartTotal() {
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = 'Total = '+'$' + total 
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+    document.getElementsByClassName('cart-total-price')[1].innerText = '$' + total;
 }
 
 
 // increase count in local storage when adding item in cart 
-function increase(){
-    var cart_count =cart_items.childElementCount
-localStorage.setItem('count', cart_count);
-// console.log(cart_count)
-    
+function increase() {
+    var cart_count = cart_items.childElementCount
+    localStorage.setItem('count', cart_count);
+    console.log(cart_count)
+
 }
 
 // decrease count in local storage when removing item in cart 
-function decrease (){
-   var cart_dec = localStorage.getItem('count')
-  
-   var new_value = parseFloat(cart_dec) -1
-//    console.log(new_value)
+function decrease() {
+    var cart_dec = localStorage.getItem('count')
+
+    var new_value = parseFloat(cart_dec) - 1
+    console.log(new_value)
     localStorage.setItem('count', new_value);
 }
 
-    // function pagesAddToCart(){
-    //     var arrOfProducts = []
-    // var newProduct= products[i]
+// function pagesAddToCart(){
+//     var arrOfProducts = []
+// var newProduct= products[i]
 
-    //             if (localStorage.products != null) {
-    //                 console.log('if ')
-    //                 arrOfProducts = JSON.parse(localStorage.getItem('products'))
-    //                 console.log(arrOfProducts);
-    //                 arrOfProducts.push(newProduct);
-    //                 console.log(arrOfProducts);
-    //                 localStorage.setItem('products',JSON.stringify(arrOfProducts) )
-    //                 // let watch = JSON.parse(localStorage.getItem('products'))
-    //                 // console.log(watch)
-    //             } else {
-    //                 console.log('else ')
-    //                 arrOfProducts.push(newProduct);
-    //                 localStorage.setItem('products',JSON.stringify(arrOfProducts) )
-    //             }
-    //             console.log(arrOfProducts)
-    //             return ;
-    //         }
+//             if (localStorage.products != null) {
+//                 console.log('if ')
+//                 arrOfProducts = JSON.parse(localStorage.getItem('products'))
+//                 console.log(arrOfProducts);
+//                 arrOfProducts.push(newProduct);
+//                 console.log(arrOfProducts);
+//                 localStorage.setItem('products',JSON.stringify(arrOfProducts) )
+//                 // let watch = JSON.parse(localStorage.getItem('products'))
+//                 // console.log(watch)
+//             } else {
+//                 console.log('else ')
+//                 arrOfProducts.push(newProduct);
+//                 localStorage.setItem('products',JSON.stringify(arrOfProducts) )
+//             }
+//             console.log(arrOfProducts)
+//             return ;
+//         }
 
+var itemsCount = localStorage.getItem('count')
+document.getElementsByClassName('items-count').innerText = itemsCount;
 
 addToCartClicked()
