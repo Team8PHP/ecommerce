085efae62a1c0f6a427bebb3ea12fcab7d60619c
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-10-01 16:49:34 +0200

    Created add to car module with its function

---
+++ js/modules/add-to-cart.js
@@ -0,0 +1,39 @@
+/****************************************************************************************************************** 
+
+*                                       Add To Cart Module
+
+****************************************************************************************************************** */
+import {isLoggedIn} from './loggedIn.js';
+
+export function addToCartBtn (product,btnID){
+    if (isLoggedIn()) {
+        //    added this code of block to add product to localstorage
+        let arrOfProducts = []
+        let newProduct = product
+        let addedBtn = document.getElementById(btnID);
+        let cart_count =document.getElementById('cart_count')
+
+        if (localStorage.products != null) {
+            arrOfProducts = JSON.parse(localStorage.getItem('products'))
+            arrOfProducts.push(newProduct);
+            localStorage.setItem('products', JSON.stringify(arrOfProducts));
+            let clicked=localStorage.getItem('countclick')
+            clicked=parseInt(clicked)+1
+            localStorage.setItem('countclick',parseInt(clicked))
+            cart_count.innerText=clicked
+            
+        } else {
+            let value = 1
+            arrOfProducts.push(newProduct);
+            localStorage.setItem('products', JSON.stringify(arrOfProducts));
+            localStorage.setItem('countclick',parseInt(value))
+            let clicked=localStorage.getItem('countclick')
+            cart_count.innerText=clicked
+        }
+        addedBtn.innerText = "Added";
+     
+    } else {
+        alert('you need to login to access your cart!');
+        window.location.href = "./login.html";
+    }
+}
